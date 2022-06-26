ad58dfa00cdb8de3efd24625231d7d00c146ef59
zhangtangzhao <751623185@qqq.com>
2022-06-26 21:41:36 +0800

    111

---
+++ editor/cn/P239_SlidingWindowMaximum .java
@@ -0,0 +1,83 @@
+//给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位
+//。 
+//
+// 返回 滑动窗口中的最大值 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+//输出：[3,3,5,5,6,7]
+//解释：
+//滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1], k = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 1 <= k <= nums.length 
+// 
+// Related Topics 队列 数组 滑动窗口 单调队列 堆（优先队列） 👍 1696 👎 0
+
+
+package leetcode.editor.cn;
+
+/**
+ * 滑动窗口最大值
+ * @author zhangtangzhao
+ * @date 2022-06-26 21:01:28
+ */
+public class P239_SlidingWindowMaximum{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P239_SlidingWindowMaximum().new Solution();
+	 }
+	 
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int n = nums.length;
+            PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+                public int compare(int[] pair1, int[] pair2) {
+                    return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+                }
+            });
+            for (int i = 0; i < k; ++i) {
+                pq.offer(new int[]{nums[i], i});
+            }
+            int[] ans = new int[n - k + 1];
+            ans[0] = pq.peek()[0];
+            for (int i = k; i < n; ++i) {
+                pq.offer(new int[]{nums[i], i});
+                while (pq.peek()[1] <= i - k) {
+                    pq.poll();
+                }
+                ans[i - k + 1] = pq.peek()[0];
+            }
+            return ans;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
