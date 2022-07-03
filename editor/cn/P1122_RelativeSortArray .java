3e58917ac6ee08a9840f3b681a502779ba8e9e35
zhangtangzhao <751623185@qqq.com>
2022-07-03 22:48:30 +0800

    111

---
+++ editor/cn/P1122_RelativeSortArray .java
@@ -0,0 +1,78 @@
+//给你两个数组，arr1 和 arr2，arr2 中的元素各不相同，arr2 中的每个元素都出现在 arr1 中。 
+//
+// 对 arr1 中的元素进行排序，使 arr1 中项的相对顺序和 arr2 中的相对顺序相同。未在 arr2 中出现过的元素需要按照升序放在 arr1 的末
+//尾。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+//输出：[2,2,2,1,4,3,3,9,6,7,19]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+//输出：[22,28,8,6,17,44]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr1.length, arr2.length <= 1000 
+// 0 <= arr1[i], arr2[i] <= 1000 
+// arr2 中的元素 arr2[i] 各不相同 
+// arr2 中的每个元素 arr2[i] 都出现在 arr1 中 
+// 
+// Related Topics 数组 哈希表 计数排序 排序 👍 225 👎 0
+
+
+package leetcode.editor.cn;
+
+/**
+ * 数组的相对排序
+ * @author zhangtangzhao
+ * @date 2022-07-03 22:44:03
+ */
+public class P1122_RelativeSortArray{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P1122_RelativeSortArray().new Solution();
+	 }
+	 
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int upper = 0;
+        for (int x : arr1) {
+            upper = Math.max(upper, x);
+        }
+        int[] frequency = new int[upper + 1];
+        for (int x : arr1) {
+            ++frequency[x];
+        }
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        for (int x : arr2) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+            frequency[x] = 0;
+        }
+        for (int x = 0; x <= upper; ++x) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
